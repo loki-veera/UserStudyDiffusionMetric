07cb127230963e1064fe5c9bdf61b8b6166e6932
loki-veera <veeramacheneni.lokesh@proton.me>
2024-02-21 14:34:35 +0100

    update GCP connection

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import os
-import toml
-import glob
 import streamlit as st
 import numpy as np
 import pandas as pd
@@ -9,15 +7,7 @@ from PIL import Image
 import time
 from copy import deepcopy
 from streamlit.runtime.scriptrunner.script_run_context import get_script_run_ctx
-from google.cloud import storage
-from google.oauth2 import service_account
-
-BUCKET_NAME="diff-test-bucket"
-CREDENTIAL_PATH="./.streamlit/secrets.toml"
-if os.path.exists(CREDENTIAL_PATH):
-    with open(CREDENTIAL_PATH, "r") as file:
-        credential_data = toml.load(file)
-CREDENTIALS = service_account.Credentials.from_service_account_info(credential_data["google_cloud"])
+from gcp_connect import GCP_Connection
 
 
 if 'hide' not in st.session_state:
@@ -38,6 +28,7 @@ if st.session_state.hide:
             Given image pairs, :red[***select the image that looks realistic***].
         """)
 
+
 if 'count' not in st.session_state:
     st.session_state.count = 0
 
@@ -47,19 +38,23 @@ if 'rn' not in st.session_state:
 if 'select' not in st.session_state:
     st.session_state.select = {}
 
+if 'gcp' not in st.session_state:
+    st.session_state.gcp = GCP_Connection(
+        bucket_name="diff-test-bucket",
+        credential_path="./.streamlit/secrets.toml"
+    )
 
-ddpm_images = sorted(glob.glob("./images/DDPM/*.jpg"))
-ddim_images = sorted(glob.glob("./images/DDIM/*.jpg"))
-ddgan_images = sorted(glob.glob("./images/DDGAN/*.jpg"))
-wave_images = sorted(glob.glob("./images/WaveDiff/*.jpg"))
+
+ddpm_images = st.session_state.gcp.get_image_names(prefix="images/DDPM/")
+ddim_images = st.session_state.gcp.get_image_names(prefix="images/DDIM/")
+ddgan_images = st.session_state.gcp.get_image_names(prefix="images/DDGAN/")
+wave_images = st.session_state.gcp.get_image_names(prefix="images/WaveDiff/")
 
 
 images = [ddpm_images, ddim_images, ddgan_images, wave_images]
+if (len(ddpm_images) == 0) or (len(ddim_images) == 0) or (len(ddgan_images) == 0) or (len(wave_images) == 0):
+    raise RuntimeError("No images found. There might be issue with GCP connection.")
 assert len(ddpm_images) == len(ddim_images) == len(ddgan_images) == len(wave_images)
-
-
-def open_image(img):
-    return Image.open(img).convert("RGB")
 
 
 def get_images():
@@ -67,13 +62,13 @@ def get_images():
     left_images = images[st.session_state.count-1]
     np.random.seed(st.session_state.rn*st.session_state.count * 1000)
     left_img_names = np.random.choice(left_images, size=len(images)-1, replace=False)
-    left_images = [open_image(name) for name in left_img_names]
+    left_images = [st.session_state.gcp.open_image(name) for name in left_img_names]
     right_images = []
     for i in range(len(images)):
         if i == st.session_state.count-1:
             continue
         np.random.seed((st.session_state.rn*st.session_state.count * i) + 10)
-        img = open_image(np.random.choice(images[i]))
+        img = st.session_state.gcp.open_image(np.random.choice(images[i]))
         right_images.append(img)
     assert len(left_images) == len(right_images)
     return left_images, right_images
@@ -127,9 +122,9 @@ def display_images():
 def next_images():
     if st.session_state.count + 1 >= len(images)+1:
         st.session_state.count = 0
-        with st.container(border=False):
+        with st.container(border=True):
             show_hide()
-            st.subheader("Thanks for taking part in the evaluation. Please click on save button to save your review.")
+            st.subheader(":red[Please click on save button to save your evaluation].")
             st.session_state.disabled=True
             if st.button("Save", key="button_save", on_click=save_csv):
                 pass
@@ -144,17 +139,13 @@ def save_csv():
     session_id = ctx.session_id
     file_name = f"answers_{session_id}.csv"
     df.to_csv(file_name, index=False)
-    write_to_cloud(file_name)
+    st.session_state.gcp.write_csv(file_name)
     os.remove(file_name)
-    st.subheader(":green[Your evaluation is saved]. Please close the tab.")
-    time.sleep(5)
-
-
-def write_to_cloud(file_name):
-    storage_client = storage.Client(credentials=CREDENTIALS)
-    bucket = storage_client.get_bucket(BUCKET_NAME)
-    blob = bucket.blob(f"user_responses/{file_name}")
-    blob.upload_from_filename(file_name)
+    with st.container(border=False):
+        st.subheader(":green[Your evaluation is saved]. Please close the tab.")
+        st.image(Image.open("./images/Thanks.png").convert("RGB"))
+    time.sleep(3)
+    del st.session_state.gcp
 
 
 if st.session_state.count >= 1:
---
+++ gcp_connect.py
@@ -0,0 +1,68 @@
+import os
+import toml
+import matplotlib.pyplot as plt
+
+from typing import List
+from google.cloud import storage
+from google.oauth2 import service_account
+from PIL import Image
+from io import BytesIO
+
+
+class GCP_Connection():
+    def __init__(self, bucket_name: str, credential_path: str) -> None:
+        """Initialize Google Cloud Platform.
+
+        Args:
+            bucket_name (str): Bucket name
+            credential_path (str): Path to google credentials
+        """
+        if os.path.exists(credential_path):
+            with open(credential_path, "r") as file:
+                credential_data = toml.load(file)
+        credentials = service_account.Credentials.from_service_account_info(credential_data["google_cloud"])
+        self.storage_client = storage.Client(credentials=credentials)
+        self.bucket = self.storage_client.get_bucket(bucket_name)
+    
+
+    def __del__(self) -> None:
+        """Close the GCP connection."""
+        self.storage_client.close()
+    
+
+    def write_csv(self, file_name: str) -> None:
+        """Write CSV file to GCP platform.
+
+        Args:
+            file_name (str): File name
+        """
+        blob = self.bucket.blob(f"user_responses/{file_name}")
+        blob.upload_from_filename(file_name)
+
+
+    def get_image_names(self, prefix: str) -> List:
+        """Get image names for specific prefix.
+
+        Args:
+            prefix (str): Specific prefix
+
+        Returns:
+            List: List containing the image names
+        """
+        img_names = [blob.name for blob in self.bucket.list_blobs(prefix=prefix)]
+        return img_names[1:] # First one is prefix itself.
+    
+
+    def open_image(self, img_name: str) -> Image.Image:
+        """Load image from GCP.
+
+        Args:
+            img_name (str): Image name
+
+        Returns:
+            Image.Image: Opened image
+        """
+        blob = self.bucket.blob(img_name)
+        blob_content = blob.download_as_bytes()
+        img = Image.open(BytesIO(blob_content))
+        return img
